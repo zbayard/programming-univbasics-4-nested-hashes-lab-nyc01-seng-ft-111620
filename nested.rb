609cea25f052b5f44402eb7dbb1ed1543851dab6
Zach Bayard <zbayard@gmail.com>
2020-10-26 19:04:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -18,6 +18,8 @@ def hopper
 
 end
 
+programmer_hash[:grace_hopper]
+
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
